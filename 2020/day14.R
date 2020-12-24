2832fd4624d7d9f4237dd51e1f9f2d76c13252e1
nmoorenz <a.message.to.nate@gmail.com>
2020-12-24 22:02:47 +1300

    finish day 7, progress on day 14

--- 2020/day14.R
+++ 2020/day14.R
@@ -1,11 +1,11 @@
+# own the libs
 library(tidyverse)
 
+# flexibility in reading
 my_file <- here::here("2020", "day14.txt")
-
 bitcode <- read.delim(my_file, header = FALSE)
 
 mem_loc <- tribble(~loc, ~orig_num, ~new_num)
-
 
 # this might not be the best way but it gets the job done
 do_mask <- function(mask, val) {
@@ -41,39 +41,57 @@ for (m in seq_len(nrow(bitcode))) {
   }
 }
 
+# the latest for each location
 mem_reduce <- mem_loc %>% 
   mutate(rn = row_number()) %>% 
   group_by(loc) %>% 
   filter(rn == max(rn)) 
 
-part_one <- Reduce(`+`, mem_reduce$new_num)
+part_one <- sum(mem_reduce$new_num)
 print(part_one, digits = 15)
 
 # 6386593869035
 
 ######################## part two
 
-
-do_mask_mem <- function(mask, mem) {
+do_mask_mem <- function(mask, mem, val) {
   mask_vector = str_split(mask, "")[[1]]
   binary_vector = rev(as.numeric(intToBits(mem)))
   # 4 extra zeros because intToBits() returns 32 bits, mask is 36
   binary_vector = append(rep(0,4), binary_vector) %>% as.character()
   
+  # tracker
+  num_x = 0
+  # loop through and make changes
+  # mask == 1, location bit = 1
+  # mask == 0, location bit unchanged
+  # mask == X, floating bit, count how many
   for (b in seq_along(mask_vector)) {
-    if (mask_vector[[b]] == "X") {
-      binary_vector[[b]] = "X"
-    } else if (mask_vector[[b]] == 1) {
+    if (mask_vector[[b]] == 1) {
       binary_vector[[b]] = 1 
-    } else {
+    } else if (mask_vector[[b]] == 0) {
       binary_vector[[b]] = binary_vector[[b]]
+    } else if (mask_vector[[b]] == "X") {
+      num_x = num_x + 1
     }
   }
-  new_val = binary_vector
-  return(new_val)
+  print(num_x)
+  # tibble the same col as what we're collecting
+  new_rows <- tribble(~orig_loc, ~loc, ~num)
+  
+  # collect the possibilities for floating bits
+  for (x in seq_len(num_x)) {
+    this_loc = Reduce(function(x,y) x*2+y, binary_vector %>% as.integer)
+    new_rows <- rbind(new_rows, list(orig_loc=mem, loc=this_loc, num=val))
+  }
+  
+  return(new_rows)
 }
 
+# set back to no locations
+mem_loc <- tribble(~orig_loc, ~loc, ~num)
 
+# loop through instructions
 for (m in seq_len(nrow(bitcode))) {
   if (str_starts(bitcode$V1[[m]], "mask")) {
     # get the mask
@@ -82,7 +100,21 @@ for (m in seq_len(nrow(bitcode))) {
     # apply mask and store value
     loc = str_extract(bitcode$V1[[m]], "\\d{4,5}")
     val = str_extract(bitcode$V1[[m]], "\\d+$")
-    new_num = do_mask_mem(mask, loc)
-    mem_loc = rbind(mem_loc, list(loc=loc, orig_num=val, new_num=new_num))
+    # instead of the new value, get back a whole dataframe
+    # there are multiple rows because of 'floating' positions
+    new_rows = do_mask_mem(mask, loc, val)
+    # bind the rows, and get the latest for each location later
+    # Might get quite large, but more efficient to stack than keep searching? 
+    mem_loc = rbind(mem_loc, new_rows)
   }
 }
+
+# the latest for each location
+mem_reduce <- mem_loc %>% 
+  mutate(rn = row_number()) %>% 
+  group_by(loc) %>% 
+  filter(rn == max(rn)) 
+
+# quite the same as part one
+part_two <- sum(mem_reduce$num)
+print(part_two, digits = 15)
--- 2020/day7.R
+++ 2020/day7.R
@@ -10,7 +10,7 @@ bag <- "shiny gold"
 
 # split
 lugg2 <- separate(lugg, V1, into = c("first", "other"), sep = " bags contain ") %>% 
-  mutate(others = as.vector(str_split(other, ",")))
+  mutate(others = as.vector(str_split(other, ", ")))
 
 lugg2$contains <- str_detect(lugg2$first, bag)
 lugg2$level <- if_else(lugg2$contains, 1, 0)
@@ -45,24 +45,69 @@ recurs_search <- function(df) {
 
 lugg3 <- recurs_search(lugg2)
 
-# part two
-srch_lvl <- 1
+################################################### part two
+
+# function to add rows to our collection dataframe
+add_bags <- function(df, item, multi) {
+  # exit early
+  if (item == "no other bags.") return(df)
+  # get the number from the string, but also the other number of bags containing this one
+  bb = parse_number(item) * multi
+  # split the string (trusting the strings are all the same)
+  cc = str_split(item, " ")
+  # combine 2nd and 3rd parts, again, trust in the system
+  clr = paste(cc[[1]][2], cc[[1]][3])
+  # the next row for our counter df
+  rw = list(bags = bb, colour = clr)
+  # otherwise if adding a number, add it to search for later
+  df = rbind(df, rw)
+  # return, of course
+  return(df)
+}
+
+# initiate some variables
 bag_current <- "1 shiny gold bag"
+keep_going <- TRUE
+bag_level <- 1
 
-parse_bags <- function(df, item) {
-  num = parse_number(item)
-  color = parse_character(item)
-  return(num)
-}
+# blank dataframe
+count_bags_df <- tribble(~bags, ~colour)
+# this is the main part of the pattern
+# loop through bags counter
+count_bags_df <- add_bags(count_bags_df, bag_current, 1)
 
+# split
+lugg_part2 <- separate(lugg, V1, into = c("first", "other"), sep = " bags contain ") %>% 
+  mutate(others = as.vector(str_split(other, ", ")))
 
-
-recurs_counter <- function(bag_srch) {
-  # look for "shiny gold"
-  srch_list <- df %>% 
-    filter(contains == TRUE & level == srch_lvl) %>% 
+# loop all of the loops
+while (keep_going) {
+  
+  # how many bags we are getting in the current one
+  multi = count_bags_df$bags[[bag_level]]
+  # current colour of the bag
+  this_bag = count_bags_df$colour[[bag_level]]
+  
+  # look for bags
+  srch_list <- lugg_part2 %>% 
+    filter(first == this_bag) %>% 
     select(others) %>% 
-    as.vector() 
+    unlist(use.names = FALSE)
+  
+  # add the found bags into our search 
+  for (x in seq_along(srch_list)) {
+    count_bags_df = add_bags(count_bags_df, srch_list[[x]], multi)
+  }
+  
+  # iterate
+  bag_level = bag_level + 1
+  
+  # stop if we have no more bags to search for
+  if (bag_level > nrow(count_bags_df)) {
+    break
+  }
 
 }
 
+# minus one because we don't count shiny gold
+sum(count_bags_df$bags) - 1
