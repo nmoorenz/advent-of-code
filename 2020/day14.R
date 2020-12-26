4d52f63026fa694e529201a7621a111577cae2af
nmoorenz <a.message.to.nate@gmail.com>
2020-12-26 23:35:49 +1300

    day 14 should work

--- 2020/day14.R
+++ 2020/day14.R
@@ -52,13 +52,12 @@ print(part_one, digits = 15)
 
 # 6386593869035
 
-######################## part two
+######################################### part two
 
-do_mask_mem <- function(mask, mem, val) {
-  mask_vector = str_split(mask, "")[[1]]
-  binary_vector = rev(as.numeric(intToBits(mem)))
+do_mask_mem <- function(bitmask, loc, val) {
+  binary_loc = rev(as.numeric(intToBits(loc)))
   # 4 extra zeros because intToBits() returns 32 bits, mask is 36
-  binary_vector = append(rep(0,4), binary_vector) %>% as.character()
+  binary_loc = append(rep(0,4), binary_loc) %>% as.character()
   
   # tracker
   num_x = 0
@@ -66,43 +65,71 @@ do_mask_mem <- function(mask, mem, val) {
   # mask == 1, location bit = 1
   # mask == 0, location bit unchanged
   # mask == X, floating bit, count how many
-  for (b in seq_along(mask_vector)) {
-    if (mask_vector[[b]] == 1) {
-      binary_vector[[b]] = 1 
-    } else if (mask_vector[[b]] == 0) {
-      binary_vector[[b]] = binary_vector[[b]]
-    } else if (mask_vector[[b]] == "X") {
+  for (b in seq_along(bitmask)) {
+    if (bitmask[[b]] == 1) {
+      binary_loc[[b]] = 1 
+    } else if (bitmask[[b]] == 0) {
+      binary_loc[[b]] = binary_loc[[b]]
+    } else if (bitmask[[b]] == "X") {
       num_x = num_x + 1
+      binary_loc[[b]] = "X" 
     }
   }
+  # for record watching
   print(num_x)
-  # tibble the same col as what we're collecting
-  new_rows <- tribble(~orig_loc, ~loc, ~num)
   
-  # collect the possibilities for floating bits
-  for (x in seq_len(num_x)) {
-    this_loc = Reduce(function(x,y) x*2+y, binary_vector %>% as.integer)
-    new_rows <- rbind(new_rows, list(orig_loc=mem, loc=this_loc, num=val))
-  }
+  # we can collapse in anticipation of str_replace
+  loc_string <- paste0(binary_loc, collapse = "")
   
+  # want to make this look nice
+  this_df <- bin_df %>% 
+    filter(num < 2 ^ num_x) %>% 
+    select(-num) %>% 
+    mutate(bin = map(bin, ~ .[(10-num_x):9]))
+  
+  # tibble to start with
+  start_rows <- tibble(loc_x = rep(loc_string, 2 ^ num_x), 
+                       bin = this_df$bin, 
+                       val = val)
+  
+  # replace X with 0 and or 1
+  loc_rows <- start_rows %>% 
+    mutate(loc = map2_chr(loc_x, bin, replace_x))
+  
+  # only want these columns
+  new_rows <- select(loc_rows, loc, val)
   return(new_rows)
 }
 
+# helper function, worried about how slow this might be
+replace_x <- function(loc_x, bin) {
+  for (z in seq_along(bin)) {
+    loc_x = str_replace(loc_x, "X", bin[z])
+  }
+  return(loc_x)
+}
+
 # set back to no locations
-mem_loc <- tribble(~orig_loc, ~loc, ~num)
+mem_loc <- tribble(~loc, ~val)
+
+# doing this to set things up
+bin_df <- tibble(num = seq(0,2^9-1))
+bin_df <- bin_df %>% mutate(bin = map(num, ~ rev(as.character(as.numeric(intToBits(.)))[1:9])))
 
 # loop through instructions
 for (m in seq_len(nrow(bitcode))) {
   if (str_starts(bitcode$V1[[m]], "mask")) {
     # get the mask
     mask = str_replace(bitcode$V1[[m]], "mask = ", "")
+    mask_vector = str_split(mask, "") %>% unlist()
   } else {
     # apply mask and store value
+    # loc = str_extract(bitcode$V1[[m]], "\\d{2}")
     loc = str_extract(bitcode$V1[[m]], "\\d{4,5}")
     val = str_extract(bitcode$V1[[m]], "\\d+$")
     # instead of the new value, get back a whole dataframe
     # there are multiple rows because of 'floating' positions
-    new_rows = do_mask_mem(mask, loc, val)
+    new_rows = do_mask_mem(mask_vector, loc, val)
     # bind the rows, and get the latest for each location later
     # Might get quite large, but more efficient to stack than keep searching? 
     mem_loc = rbind(mem_loc, new_rows)
@@ -116,5 +143,31 @@ mem_reduce <- mem_loc %>%
   filter(rn == max(rn)) 
 
 # quite the same as part one
-part_two <- sum(as.numeric(mem_reduce$num))
+part_two <- sum(as.numeric(mem_reduce$val))
 print(part_two, digits = 15)
+
+# 4288985857999
+
+
+
+
+
+
+
+
+
+# check the number of rows is good
+# the numbers are what I expect: 77232
+
+num_rows <- 0
+
+# loop through instructions
+for (m in seq_len(nrow(bitcode))) {
+  if (str_starts(bitcode$V1[[m]], "mask")) {
+    mask = str_replace(bitcode$V1[[m]], "mask = ", "")
+  } else {
+    num_x = str_count(mask, "X")
+    this_rows = 2 ^ num_x
+    num_rows = num_rows + this_rows 
+  }
+}
--- 2020/day18.R
+++ 2020/day18.R
@@ -3,58 +3,146 @@ library(tidyverse)
 my_file <- here::here("2020", "day18.txt")
 equations <- read.delim(my_file, header = FALSE)
 
-do_op <- function(){
-  
+do_op <- function(so_far, op, x){
+  if (op == "") {
+    so_far = so_far + as.numeric(x)
+  } else if (op == 'add') {
+    so_far = so_far + as.numeric(x)
+  } else if (op == 'mlt') {
+    so_far = so_far * as.numeric(x)
+  }
+  return(so_far)
 }
 
 eval_equation <- function(eq) {
   # inititate total
   so_far = 0
   op = ""
-  for (e in seq_len(nchar(eq))) {
+  while (e < nchar(eq)) {
+    e <<- e + 1
     x = substr(eq, e, e)
+    
+    # print(e)
+    # print(x)
     
     if (x == " ") {
       # skip
-      next
+      
     } else if (x == "(") {
       # recursion
-      print('recursion')
-      rec = eval_equation(substr(eq, e+1, 1e6))
+      # print('recursion')
+      rec = eval_equation(eq)
       so_far = do_op(so_far, op, rec)
+      
     } else if (x == ")") {
       # close the recursion
       # return inside total
-      print('return')
+      # print('return')
+      return(so_far)
+      
     } else if (x == "+") {
       # add
-      print('add')
+      # print('add')
       op = 'add'
+      
     } else if (x == "*") {
       # multiply
-      print('multiply')
+      # print('multiply')
       op = 'mlt'
+      
     } else {
-      print(x)
-      if (op == "") {
-        so_far = so_far + as.numeric(x)
-      } else if (op == 'add') {
-        so_far = so_far + as.numeric(x)
-      } else if (op == 'mlt') {
-        so_far = so_far * as.numeric(x)
-      }
+      # trusting that this is a number
+      # print(x)
+      so_far = do_op(so_far, op, x)
     }
-    print(so_far)
+    # print(so_far)
+    # print('---')
   }
   return(so_far)
 }
 
-
-tester = "1 + (2 * 3) + (4 * (5 + 6))"
-
+tester = "8 * (9 + 5 + 5 * 6 + 8 * 3) * 5 * 7 * 4 + 9"
+e <<- 0
 eval_equation(tester)
 
+# need a wrapper function to reset e <<- 0
+calc_line <- function(eq) {
+  e <<- 0
+  eval_equation(eq)
+}
 
+equated <- equations %>% 
+  mutate(line_sum = map_dbl(V1, calc_line))
+
+# part one
+as.character(sum(equated$line))
+
+################################### part two
+
+beasmd <- function(eq) {
+  # inititate total
+  so_far = 0
+  op = ""
+  while (e < nchar(eq)) {
+    e <<- e + 1
+    x = substr(eq, e, e)
+    
+    print(e)
+    print(x)
+    
+    if (x == " ") {
+      # skip
+      
+    } else if (x == "(") {
+      # recursion
+      print('recursion')
+      rec = eval_equation(eq)
+      so_far = do_op(so_far, op, rec)
+      
+    } else if (x == ")") {
+      # close the recursion
+      # return inside total
+      print('return')
+      return(so_far)
+      
+    } else if (x == "+") {
+      # add
+      # print('add')
+      op = 'add'
+      
+    } else if (x == "*") {
+      # multiply
+      # print('multiply')
+      op = 'mlt'
+      rec = eval_equation(eq)
+      so_far = do_op(so_far, op, rec)
+      
+    } else {
+      # trusting that this is a number
+      print(x)
+      so_far = do_op(so_far, op, x)
+    }
+    print(so_far)
+    print('---')
+  }
+  return(so_far)
+}
+
+tester = "1 + 2 * 3 + 4 * 5 + 6"
+e <<- 0
+beasmd(tester)
+
+# need a wrapper function to reset e <<- 0
+calc_line_2 <- function(eq) {
+  e <<- 0
+  beasmd(eq)
+}
+
+equate_part_two <- equations %>% 
+  mutate(line_2 = map_dbl(V1, calc_line_2))
+
+# part two
+as.character(sum(equated$line))
 
 ############ example
 "
