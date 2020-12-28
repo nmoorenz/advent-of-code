44f29ca36a128e79abd6f86a392060581ca681d4
nmoorenz <a.message.to.nate@gmail.com>
2020-12-28 13:05:46 +1300

    changed the arithmetic functions for fake operator precedence

--- 2020/day18.R
+++ 2020/day18.R
@@ -79,70 +79,33 @@ as.character(sum(equated$line))
 
 ################################### part two
 
-beasmd <- function(eq) {
-  # inititate total
-  so_far = 0
-  op = ""
-  while (e < nchar(eq)) {
-    e <<- e + 1
-    x = substr(eq, e, e)
-    
-    print(e)
-    print(x)
-    
-    if (x == " ") {
-      # skip
-      
-    } else if (x == "(") {
-      # recursion
-      print('recursion')
-      rec = eval_equation(eq)
-      so_far = do_op(so_far, op, rec)
-      
-    } else if (x == ")") {
-      # close the recursion
-      # return inside total
-      print('return')
-      return(so_far)
-      
-    } else if (x == "+") {
-      # add
-      # print('add')
-      op = 'add'
-      
-    } else if (x == "*") {
-      # multiply
-      # print('multiply')
-      op = 'mlt'
-      rec = eval_equation(eq)
-      so_far = do_op(so_far, op, rec)
-      
-    } else {
-      # trusting that this is a number
-      print(x)
-      so_far = do_op(so_far, op, x)
-    }
-    print(so_far)
-    print('---')
-  }
-  return(so_far)
+`/` <- function(a, b) {
+  a + b
 }
 
-tester = "1 + 2 * 3 + 4 * 5 + 6"
-e <<- 0
-beasmd(tester)
-
-# need a wrapper function to reset e <<- 0
-calc_line_2 <- function(eq) {
-  e <<- 0
-  beasmd(eq)
+`-` <- function(a, b) {
+  a * b
 }
 
+replace_symbol <- function(strng) {
+  # swap precendence
+  strng = str_replace_all(strng, "\\*", "-")
+  strng = str_replace_all(strng, "\\+", "/")
+  return(strng)
+}
+
+# test on individual function
+tester <- "1 + 2 * 3 + 4 * 5 + 6"
+test2 <- replace_symbol(tester)
+eval(parse(text = test2))
+
+# change the thing we want to calculate
 equate_part_two <- equations %>% 
-  mutate(line_2 = map_dbl(V1, calc_line_2))
+  mutate(line_2 = replace_symbol(V1), 
+         eq_2 = map_dbl(line_2, ~ eval(parse(text = .))))
 
 # part two
-as.character(sum(equated$line))
+as.character(sum(equate_part_two$eq_2))
 
 ############ example
 "
