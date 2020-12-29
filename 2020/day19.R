f575a79dc75514eca41c2957076b2094a6798da2
nmoorenz <a.message.to.nate@gmail.com>
2020-12-29 23:21:23 +1300

    I don't like 19, 20 is ok so far, 21 was nice

--- 2020/day19.R
+++ 2020/day19.R
@@ -1,5 +1,70 @@
+library(tidyverse)
 
-my_file <- here::here("2020", "day18.txt")
-equations <- read.delim(my_file, header = FALSE)
-cards <- readLines(my_file)
+my_file <- here::here("2020", "day19.txt")
+r_m <- read.delim(my_file, header = FALSE)
+
+# separate into rules and messages
+r_m <- r_m %>% 
+  mutate(rule = str_detect(V1, ":"))
+
+# get those objects
+rules <- filter(r_m, rule == TRUE) %>% 
+  select(-rule)
+
+messages <- filter(r_m, rule == FALSE) %>% 
+  select(-rule) 
+
+# parse some rules
+rule_sep <- rules %>% 
+  separate(V1, into = c("rule_num", "others"), sep = ": ") 
+
+# make a copy
+rule_sub <- rule_sep %>% 
+  mutate(done = FALSE)
+
+# run repeatedly below
+# i think we want to replace when there are all letters in the rule
+rule_sub <- rule_sub %>% 
+  mutate(any_num = str_detect(others, "\\d")) %>% 
+  arrange(rule_num)
+
+# get the rules we want to change
+rr <- rule_sub %>% 
+  filter(any_num == FALSE & done == FALSE)
+
+# apparently we need a function to apply the substitution
+# otherwise doesn't pick up boundaries properly
+replace_func <- function(my_list, patt, repl) {
+  str_replace_all(my_list, patt, repl)
+}
+
+# now make the changes
+for (z in seq_len(nrow(rr))) {
+  patt = paste0("\\b", rr$rule_num[[z]], "\\b")
+  repl = paste0("(", rr$others[[z]], ")")
+  rule_sub <- rule_sub %>% mutate(others = map(others, replace_func, patt, repl), 
+                                  done = done | str_detect(rule_num, paste0("\\b", rr$rule_num[[z]], "\\b")))
+  
+}
+
+
+# I'm not sure how to simplify this process to come up with coherent rules
+# we want to make this quicker by recording which rules we've already done
+# we might want to only work with one df in a loop
+# just run the above a few times and that'll do
+
+# part one: just check rule zero
+
+rule_0 <- rule_sub %>% 
+  filter(rule_num == "0") %>% 
+  select(others) %>% 
+  unlist(use.names = FALSE)
+
+# ah goodness it is huge
+
+rule_0_tight <- str_replace_all(rule_0, "\\(([ab])\\) \\(([ab])\\)", "\\1\\2")
+# (a) (b)
+rule_0_tight <- str_replace_all(rule_0_tight, "\\(([ab]+)\\) \\(([ab]+)\\)", "\\1\\2")
+# (a) | (b)
+rule_0_tight <- str_replace_all(rule_0_tight, "\\(([ab]+)\\) \\| \\(([ab]+)\\)", "\\1\\|\\2")
 
--- 2020/day20.R
+++ 2020/day20.R
@@ -1,5 +1,79 @@
+library(tidyverse)
 
-my_file <- here::here("2020", "day18.txt")
-equations <- read.delim(my_file, header = FALSE)
-cards <- readLines(my_file)
+# data
+my_file <- here::here("2020", "day20.txt")
+tiles <- read.delim(my_file, header = FALSE)
 
+# names of the tiles
+tile_names <- tiles %>% 
+  mutate(names = str_detect(V1, "Tile")) %>% 
+  filter(names == TRUE) %>% 
+  select(-names) %>% 
+  mutate(tile_num = parse_number(V1))
+
+# join back the names then create nested item
+tile_piece <- tiles %>% 
+  left_join(tile_names, by = "V1") %>% 
+  fill(tile_num) %>% 
+  filter(str_detect(V1, "Tile", negate = TRUE)) %>% 
+  group_by(tile_num) %>% 
+  nest(data = c(V1))
+
+# a little work to get the edges, and in reverse
+get_edges <- function(piece) {
+  # first row
+  r1a = piece[[1]][1]
+  r1b = stringi::stri_reverse(r1a)
+  # last row
+  r10a = piece[[1]][10]
+  r10b = stringi::stri_reverse(r10a)
+  # first column and last column
+  c1a = ""
+  c10a = ""
+  for (i in seq_len(10)) {
+    c1a = str_c(c1a, str_sub(piece[[1]][i], 1, 1))
+    c10a = str_c(c10a, str_sub(piece[[1]][i], 10, 10))
+  }
+  # and the reversal
+  c1b = stringi::stri_reverse(c1a)
+  c10b = stringi::stri_reverse(c10a)
+  # create a list of these things
+  edges = c(r1a, r1b, r10a, r10b, c1a, c1b, c10a, c10b)
+  return(edges)
+}
+
+# possibilities for the edges of the tiles
+tile_edges <- tile_piece %>% 
+  mutate(edges = map(data, get_edges))
+
+edge_checker <- tile_edges %>% 
+  unnest_longer(edges)
+
+# function to compare edges
+# if we can find more than two, it's not a corner
+# if there are only two matches, it is a corner
+# for now, just looking for corners so count those
+match_edges <- function(my_edges, num) {
+  mm = vector(mode = "integer")
+  for (e in seq_along(my_edges)) {
+    maybe = edge_checker %>% 
+      filter(edges == my_edges[[e]], tile_num != num)
+    # if (nrow(maybe) > 0) {
+    #   yesyes = maybe %>% 
+    #     select(tile_num) %>% 
+    #     unlist(use.names = FALSE)
+    #   mm = append(mm, yesyes)
+    # }
+    mm = append(mm, nrow(maybe))
+  }
+  return(mm)
+}
+
+tile_matches <- tile_edges %>% 
+  mutate(matcher = map2(edges, tile_num, match_edges))
+
+tile_corners <- tile_matches %>% 
+  mutate(match_num = map_dbl(matcher, sum) / 2) %>% 
+  filter(match_num ==2)
+
+as.character(prod(tile_corners$tile_num))
