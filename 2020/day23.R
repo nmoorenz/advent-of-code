bb9f0be11a07c0696f64ea05080c0dc3ffcd1c0a
nmoorenz <a.message.to.nate@gmail.com>
2021-01-04 11:15:46 +1300

    Done! day 23 complete

--- 2020/day23.R
+++ 2020/day23.R
@@ -59,7 +59,7 @@ part_one = '29385746'
 ###################### part two
 
 # number of cups
-cup_max <- 1e3
+cup_max <- 1e6
 # sequential cups for anything more than the original
 extra_cups <- seq(10, cup_max)
 # original input
@@ -68,32 +68,24 @@ orig_cups <- str_split('712643589', "")[[1]] %>% as.integer()
 cups <- c(orig_cups, extra_cups)
 # next cups
 next_cups <- c(cups[-1], cups[1])
+# cup directory
+cup_pos <- next_cups[order(cups)]
 
 # large number of moves
 moves <- cup_max * 10
 checker = seq(moves/100, moves/100*99, by = moves/100)
 
-# this is the controlling thing
-ind <- 1
+# current cup
+current_cup = cups[1]
 
 # write the code then wrap in a loop
 for (i in seq_len(moves)) {
-  # current cup
-  current_cup <- cups[ind]
-  
-  # to the right of the current cup
-  # needs to wrap around when we get to the end
-  # maybe two vectors is the way to go
-  three_cups <- c(cups, cups[1:10])[(ind+1):(ind+3)]
-  
   if (i %in% checker) print(i)
   
-  # this preserves the order as far as I can tell
-  if (ind <= cup_max - 4) {
-    cups <- c(cups[1:ind], cups[(ind+4):cup_max])
-  } else {
-    cups <- cups[(ind-cup_max+4):ind]
-  }
+  # pickup cups
+  p1 = cup_pos[current_cup]
+  p2 = cup_pos[p1]
+  p3 = cup_pos[p2]
   
   # destination, one less than the current cup
   good = FALSE
@@ -103,24 +95,30 @@ for (i in seq_len(moves)) {
     if (dest_cup == 0) {
       dest_cup <- cup_max
     }
-    if (!(dest_cup %in% three_cups)) {
+    if (!(dest_cup %in% c(p1, p2, p3))) {
       good = TRUE
     }
   }
   
-  # location of the destination cup
-  dest_loc <- cup_pos[dest_cup]
-  if (ind < dest_loc) dest_loc = dest_loc - 3
-    
-  # need to add the three cups back into the vector
-  cups <- append(cups, three_cups, after = dest_loc)
+  cup_pos[current_cup] <- cup_pos[p3]
+  # current now points to where third cup was pointing, removing three cups from loop
   
-  ind <- match(current_cup, cups) + 1
-  if (ind == cup_max + 1) {
-    ind <- 1
-  }
+  cup_pos[p3] <- cup_pos[dest_cup]
+  # third now points to where destination cup was pointing
+  
+  cup_pos[dest_cup] <- p1
+  # destination cup now points at the first cup in the pickup
+  
+  current_cup <- cup_pos[current_cup]
+  # move pointer to where the current cup is pointing
   
 }
+
+# I don't know how this works honestly
+x1 <- cup_pos[1]
+x2 <- cup_pos[x1]
+
+prod(x1,x2)
 
 # 1,000 cups and 10,000 moves takes about two seconds
 # 10,000 cups and 100,000 moves takes about one minute
@@ -132,6 +130,9 @@ for (i in seq_len(moves)) {
 # replacing the setdiff() removed 10 seconds from 10,000 cups time
 # replacing the match for destination location removed a further 15 seconds
 # don't get the same answer though
+
+# thanks Tan
+# https://tanho63.github.io/advent_of_code/2020/day-23
 
 "
 The crab picks up the three cups that are immediately clockwise of the current cup. 
