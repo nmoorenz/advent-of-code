36f627d079c3610ce034f3548e38202c823f9ff8
nmoorenz <a.message.to.nate@gmail.com>
2023-11-28 22:49:29 +1300

    update template file, create-files script, create 2023 files

--- create-files.py
+++ create-files.py
@@ -1,19 +1,22 @@
 
+# do this once per year 
+yr = '2023'
+
 # loop through and create text files
-for i in range(13, 26):
-    with open(f"2015/data-2015-{i:02d}.txt", "w") as f:
+for i in range(1, 26):
+    with open(f"{yr}/data-{yr}-{i:02d}.txt", "w") as f:
         pass
 
-
-with open('2015/2015-template.qmd', 'r') as f:
+# read in the template
+with open(f'template.qmd', 'r') as f:
     qmd_file = f.read()
 
+# use the template with some variables
+for i in range(1, 26):
+    with open(f"{yr}/{yr}-{i:02d}.qmd", "w") as f:
+        f.write(qmd_file.format(x=i, yr=yr, python="{python}"))
 
-for i in range(13, 26):
-    with open(f"2015/2015-{i:02d}.qmd", "w") as f:
-        f.write(qmd_file.format(x=i, python="{python}"))
-
-
+# create the overall readme file with notes for each day and part
 readme = """
 
 ### Day {i}
@@ -26,6 +29,8 @@ readme = """
 
 """
 
-with open('2015/README.md', 'a') as f:
-    for i in range(13, 26):
+# pre-formatted readme to describe each day's attempt
+with open(f'{yr}/README.md', 'a') as f:
+    f.write(f"## Advent of Code {yr}")
+    for i in range(1, 26):
         f.write(readme.format(i=i))
